3b09ee761c9ed646c191cc1eda8bfa576059d9a8
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-12-10 09:19:54 -0800

    Added some validation

--- DoorSmash/Product.cs
+++ DoorSmash/Product.cs
@@ -10,6 +10,7 @@ namespace DoorSmash
     class Product
     {
         private string name;
+        private double price;
 
         public Product(String productName)
         {
@@ -28,21 +29,34 @@ namespace DoorSmash
         public string Name //Select the wrench, and select full property
         {
             get { return name; }
-            set 
-            {   
-                if(string.IsNullOrWhiteSpace(value)) //essentially if it is null
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) //essentially if it is null
                 {
                     throw new ArgumentException($"{nameof(Name)} cannot be null");
                 }
-                if(value.Length > 50) //business logic only
+                if (value.Length > 50) //business logic only
                 {//nameof is a way to get the property
                     throw new ArgumentException($"{nameof(Name)}Name cannot have more than 50 characters");
                 }
-                name = value; 
+                name = value;
             }
         }
 
-        public double Price { get; set; }
+        public double Price { //when you automatically change the property, get rid of lambda and format like so
+            get {
+                return price;
+            }
+            set 
+            {
+                if(value < 0)
+                {
+                    string msg = $"{nameof(Price)} must be a postive value";
+                    throw new ArgumentOutOfRangeException(msg);
+                }
+                price = value; 
+            }
+        }
 
         public string Description { get; set; }
 
